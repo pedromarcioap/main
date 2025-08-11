05cc7c7f60e47f685ef65b956dbd3f5cf82f82b7
Pedro Márcio Di Pietro <pedromarcioap@gmail.com>
2025-08-11 15:31:10 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,16 +1,16 @@
 'use client';
 
-import { initializeApp, getApps, getApp } from 'firebase/app';
+import { initializeApp, getApps, getApp, type FirebaseOptions } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
-const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+const firebaseConfig: FirebaseOptions = {
+  apiKey: "AIzaSyBZlz6yRqt4rLUTmiFKWOrMjo1PDpMbBG8",
+  authDomain: "izybotanic.firebaseapp.com",
+  projectId: "izybotanic",
+  storageBucket: "izybotanic.firebasestorage.app",
+  messagingSenderId: "648909338628",
+  appId: "1:648909338628:web:e5000b690e4b3d37675d51",
 };
 
 // Initialize Firebase
