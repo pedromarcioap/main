9f421d29e40940c69a7c1247cbcdc129535c21e4
Pedro Márcio Di Pietro <pedromarcioap@gmail.com>
2025-08-11 15:42:36 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,12 +5,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig: FirebaseOptions = {
-  apiKey: "AIzaSyBZlz6yRqt4rLUTmiFKWOrMjo1PDpMbBG8",
-  authDomain: "izybotanic.firebaseapp.com",
-  projectId: "izybotanic",
-  storageBucket: "izybotanic.firebasestorage.app",
-  messagingSenderId: "648909338628",
-  appId: "1:648909338628:web:e5000b690e4b3d37675d51",
+    apiKey: "AIzaSyBZlz6yRqt4rLUTmiFKWOrMjo1PDpMbBG8",
+    authDomain: "izybotanic.firebaseapp.com",
+    projectId: "izybotanic",
+    storageBucket: "izybotanic.appspot.com",
+    messagingSenderId: "648909338628",
+    appId: "1:648909338628:web:e5000b690e4b3d37675d51"
 };
 
 // Initialize Firebase
