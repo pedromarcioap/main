fe14f6d5c2888f5c6073b83d718edf4a0c3bf1e9
Pedro Márcio Di Pietro <pedromarcioap@gmail.com>
2025-08-12 01:01:29 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,12 +5,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig: FirebaseOptions = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    projectId: "izybotanic",
+    appId: "1:648909338628:web:e5000b690e4b3d37675d51",
+    storageBucket: "izybotanic.firebasestorage.app",
+    apiKey: "AIzaSyBZlz6yRqt4rLUTmiFKWOrMjo1PDpMbBG8",
+    authDomain: "izybotanic.firebaseapp.com",
+    messagingSenderId: "648909338628",
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
