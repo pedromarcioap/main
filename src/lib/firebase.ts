62853bfdeb1535002577316feb6bae40c01b67bf
Pedro Márcio Di Pietro <pedromarcioap@gmail.com>
2025-08-14 02:41:38 +0000

    O app não está abrindo nem renderizando

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,39 +2,39 @@
 
 import { initializeApp, getApps, getApp, type FirebaseOptions } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-import { getFirestore, initializeFirestore } from 'firebase/firestore';
+import { getFirestore, initializeFirestore, memoryLocalCache, persistentLocalCache } from 'firebase/firestore';
 
 const firebaseConfig: FirebaseOptions = {
     apiKey: "AIzaSyBZlz6yRqt4rLUTmiFKWOrMjo1PDpMbBG8",
     authDomain: "izybotanic.firebaseapp.com",
     projectId: "izybotanic",
-    storageBucket: "izybotanic.firebasestorage.app",
+    storageBucket: "izybotanic.appspot.com",
     messagingSenderId: "648909338628",
     appId: "1:648909338628:web:e5000b690e4b3d37675d51",
     measurementId: "G-ZBQTLG0ERH"
 };
 
 
-// Inicializa o Firebase App
+// Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
-
-// Inicializa o Firebase Auth
 const auth = getAuth(app);
 
-// Inicializa o Firestore com persistência offline
+// Initialize Firestore with persistent storage
 let db;
-if (typeof window !== 'undefined') {
-  try {
-    db = initializeFirestore(app, {
-      localCache: { kind: 'persistent' },
-    });
-    console.log('Firestore initialized with persistent cache.');
-  } catch (error) {
-    console.error('Error initializing persistent cache, falling back to in-memory.', error);
-    db = getFirestore(app);
-  }
-} else {
-  db = getFirestore(app);
+try {
+   db = initializeFirestore(app, {
+    localCache: persistentLocalCache({
+      tabManager: {
+        forceOwnership: true,
+      }
+    }),
+  });
+} catch(e) {
+  console.error(e);
+  db = initializeFirestore(app, {
+    localCache: memoryLocalCache({})
+  })
 }
+
 
 export { app, auth, db };
